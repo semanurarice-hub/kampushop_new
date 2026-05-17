b066b64fe329c71df8cfdd4e2c39331e95a8191b
Semanur Arı <semanur.ari.ce@gmail.com>
2026-05-17 23:46:12 +0300

    Delete web_proje directory

--- web_proje/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-$conn = new mysqli("localhost", "root", "", "kampushop");
-
-if ($conn->connect_error) {
-    die("Bağlantı hatası: " . $conn->connect_error);
-}
-
-echo "BAĞLANTI TAMAM";
-?>
